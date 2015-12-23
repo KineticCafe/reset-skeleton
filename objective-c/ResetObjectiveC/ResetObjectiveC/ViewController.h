98474232466f36fe24a78854e9c679a49cfc3575
Carlos Conejo <cconejo@kineticcafe.com>
2015-12-23 15:38:42 -0500

    Added the ViewController

---
+++ objective-c/ResetObjectiveC/ResetObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResetObjectiveC
+//
+//  Created by Carlos Conejo on 2015-12-23.
+//  Copyright © 2015 Kinetic Cafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ objective-c/ResetObjectiveC/ResetObjectiveC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ResetObjectiveC
+//
+//  Created by Carlos Conejo on 2015-12-23.
+//  Copyright © 2015 Kinetic Cafe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
